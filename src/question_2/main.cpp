5272719eb4f0292054fddd98c22325f8c142ba46
lanefrasier22 <lane.frasier@g.austincc.edu>
2022-10-11 20:38:05 +0000

    question 2 Not finished

--- src/question_2/main.cpp
+++ src/question_2/main.cpp
@@ -1,6 +1,26 @@
+#include "question2.h"
+#include <iostream>
+using std::cout;
+using std::cin;
+
 
 int main()
 {
+    int num;
+    std::string output;
+    while(true){
+        cout<<"Please enter an integer between 1 and 255 or press 00 to exit: ";
+        cin>>num;
+        if(num == 00){
+            break;
+        }
+        while((num < 1 || num > 255) && num != 00){
+        cout<<"Out of bounds. Please enter an integer between 1 and 255: ";
+        }
+        output = decimal_to_binary(num);
+        cout<<"Here is the binary string: "<<output;
+        }
 
     return 0;
-}
+    }
+    
--- src/question_2/question2.cpp
+++ src/question_2/question2.cpp
@@ -1,6 +1,14 @@
 #include "question2.h"
+#include <string>
 
-bool test_config()
-{
-    return true;
+
+std::string decimal_to_binary(int num)
+{   
+    std::string str;
+
+    while ( num!=0 ){
+        str += ( num % 2 == 0 ? "0" : "1" );
+        num /= 2;
+    }
+
 }
